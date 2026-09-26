b388fa5d2847b3fae39a595148ee0beb5fa02372
GutGutGutGut <komitunakamad4@gmail.com>
2026-09-25 22:41:34 -0300

    pqxdh v3

--- src/handshake/pqxdh.c
+++ src/handshake/pqxdh.c
@@ -1,216 +1,699 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "pqxdh.h"
 
 #include "../crypto/crypto.h"
 #include "../crypto/mlkem.h"
 #include "../crypto/x25519.h"
+#include "../crypto/xeddsa.h"
 
+#include <openssl/evp.h>
+#include <openssl/kdf.h>
+
+#include <stddef.h>
 #include <string.h>
+
+#define GYROJET_PQXDH_DOMAIN \
+    "GyroJett-OneShot2_CURVE25519_SHA-256_MLKEM-768"
+
+#define GYROJET_PQXDH_DOMAIN_SIZE \
+    (sizeof(GYROJET_PQXDH_DOMAIN) - 1)
+
+#define GYROJET_PQXDH_F_SIZE 32
+
+static int gyrojet_pqxdh_kdf(
+    const unsigned char *key_material,
+    size_t key_material_size,
+    unsigned char output[
+        GYROJET_PQXDH_SHARED_SECRET_SIZE
+    ]
+)
+{
+    if (key_material == NULL ||
+        output == NULL) {
+        return -1;
+    }
+
+    /*
+     * PQXDH:
+     *
+     * HKDF-IKM = F || KM
+     *
+     * F = 32 bytes 0xff para Curve25519.
+     *
+     * HKDF salt = 32 bytes zero.
+     */
+
+    if (key_material_size >
+        SIZE_MAX - GYROJET_PQXDH_F_SIZE) {
+        return -1;
+    }
+
+    const size_t ikm_size =
+        GYROJET_PQXDH_F_SIZE +
+        key_material_size;
+
+    unsigned char ikm[
+        GYROJET_PQXDH_F_SIZE +
+        4 * GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
+
+    unsigned char salt[32];
+
+    memset(
+        ikm,
+        0,
+        sizeof(ikm)
+    );
+
+    memset(
+        salt,
+        0,
+        sizeof(salt)
+    );
+
+    memset(
+        ikm,
+        0xff,
+        GYROJET_PQXDH_F_SIZE
+    );
+
+    memcpy(
+        ikm + GYROJET_PQXDH_F_SIZE,
+        key_material,
+        key_material_size
+    );
+
+    EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(
+        EVP_PKEY_HKDF,
+        NULL
+    );
+
+    if (ctx == NULL) {
+        gyrojet_crypto_secure_zero(
+            ikm,
+            sizeof(ikm)
+        );
+
+        gyrojet_crypto_secure_zero(
+            salt,
+            sizeof(salt)
+        );
+
+        return -1;
+    }
+
+    int result = -1;
+
+    if (EVP_PKEY_derive_init(ctx) != 1)
+        goto cleanup;
+
+    if (EVP_PKEY_CTX_set_hkdf_md(
+            ctx,
+            EVP_sha256()
+        ) != 1) {
+        goto cleanup;
+    }
+
+    if (EVP_PKEY_CTX_set1_hkdf_salt(
+            ctx,
+            salt,
+            (int)sizeof(salt)
+        ) != 1) {
+        goto cleanup;
+    }
+
+    if (EVP_PKEY_CTX_set1_hkdf_key(
+            ctx,
+            ikm,
+            (int)ikm_size
+        ) != 1) {
+        goto cleanup;
+    }
+
+    if (EVP_PKEY_CTX_add1_hkdf_info(
+            ctx,
+            (unsigned char *)GYROJET_PQXDH_DOMAIN,
+            (int)GYROJET_PQXDH_DOMAIN_SIZE
+        ) != 1) {
+        goto cleanup;
+    }
+
+    size_t output_size =
+        GYROJET_PQXDH_SHARED_SECRET_SIZE;
+
+    if (EVP_PKEY_derive(
+            ctx,
+            output,
+            &output_size
+        ) != 1) {
+        goto cleanup;
+    }
+
+    if (output_size !=
+        GYROJET_PQXDH_SHARED_SECRET_SIZE) {
+        goto cleanup;
+    }
+
+    result = 0;
+
+cleanup:
+
+    if (result != 0) {
+        gyrojet_crypto_secure_zero(
+            output,
+            GYROJET_PQXDH_SHARED_SECRET_SIZE
+        );
+    }
+
+    EVP_PKEY_CTX_free(ctx);
+
+    gyrojet_crypto_secure_zero(
+        ikm,
+        sizeof(ikm)
+    );
+
+    gyrojet_crypto_secure_zero(
+        salt,
+        sizeof(salt)
+    );
+
+    return result;
+}
+
+static int gyrojet_pqxdh_verify_prekeys(
+    const gyrojet_identity_t *remote_identity
+)
+{
+    if (remote_identity == NULL)
+        return -1;
+
+    if (gyrojet_xeddsa_verify(
+            remote_identity->identity_key.public_key,
+            remote_identity->signed_prekey.public_key,
+            GYROJET_X25519_PUBLIC_KEY_SIZE,
+            remote_identity->signed_prekey_signature
+        ) != 0) {
+        return -1;
+    }
+
+    if (gyrojet_xeddsa_verify(
+            remote_identity->identity_key.public_key,
+            remote_identity->pq_signed_prekey.public_key,
+            GYROJET_MLKEM_PUBLIC_KEY_SIZE,
+            remote_identity->pq_signed_prekey_signature
+        ) != 0) {
+        return -1;
+    }
+
+    return 0;
+}
 
 int gyrojet_pqxdh_init(void)
 {
-return gyrojet_crypto_init();
+    return gyrojet_crypto_init();
 }
 
-int gyrojet_pqxdh_run_test(
-gyrojet_pqxdh_result_t *result
+int gyrojet_pqxdh_initiate(
+    const gyrojet_identity_t *local_identity,
+    const gyrojet_identity_t *remote_identity,
+    gyrojet_pqxdh_message_t *message,
+    gyrojet_pqxdh_result_t *result
 )
 {
-if (result == NULL)
-return -1;
+    if (local_identity == NULL ||
+        remote_identity == NULL ||
+        message == NULL ||
+        result == NULL) {
+        return -1;
+    }
 
-memset(
-    result,
-    0,
-    sizeof(*result)
-);
+    memset(
+        message,
+        0,
+        sizeof(*message)
+    );
 
-unsigned char alice_private[
-    GYROJET_X25519_PRIVATE_KEY_SIZE
-];
+    memset(
+        result,
+        0,
+        sizeof(*result)
+    );
 
-unsigned char alice_public[
-    GYROJET_X25519_PUBLIC_KEY_SIZE
-];
+    unsigned char ephemeral_private[
+        GYROJET_X25519_PRIVATE_KEY_SIZE
+    ];
 
-unsigned char bob_private[
-    GYROJET_X25519_PRIVATE_KEY_SIZE
-];
+    unsigned char dh1[
+        GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
 
-unsigned char bob_public[
-    GYROJET_X25519_PUBLIC_KEY_SIZE
-];
+    unsigned char dh2[
+        GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
 
-unsigned char alice_dh[
-    GYROJET_X25519_SHARED_SECRET_SIZE
-];
+    unsigned char dh3[
+        GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
 
-unsigned char bob_dh[
-    GYROJET_X25519_SHARED_SECRET_SIZE
-];
+    unsigned char pq_secret[
+        GYROJET_MLKEM_SHARED_SECRET_SIZE
+    ];
 
-unsigned char bob_mlkem_public[
-    GYROJET_MLKEM_PUBLIC_KEY_SIZE
-];
+    unsigned char key_material[
+        4 * GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
 
-unsigned char bob_mlkem_private[
-    GYROJET_MLKEM_PRIVATE_KEY_SIZE
-];
+    unsigned char signing_public_key[
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    ];
 
-unsigned char ciphertext[
-    GYROJET_MLKEM_CIPHERTEXT_SIZE
-];
+    memset(ephemeral_private, 0, sizeof(ephemeral_private));
+    memset(dh1, 0, sizeof(dh1));
+    memset(dh2, 0, sizeof(dh2));
+    memset(dh3, 0, sizeof(dh3));
+    memset(pq_secret, 0, sizeof(pq_secret));
+    memset(key_material, 0, sizeof(key_material));
+    memset(signing_public_key, 0, sizeof(signing_public_key));
 
-unsigned char alice_mlkem_secret[
-    GYROJET_MLKEM_SHARED_SECRET_SIZE
-];
+    int result_code = -1;
 
-unsigned char bob_mlkem_secret[
-    GYROJET_MLKEM_SHARED_SECRET_SIZE
-];
+    /*
+     * O iniciador verifica primeiro os prekeys
+     * autenticados pelo identity key remoto.
+     */
+    if (gyrojet_pqxdh_verify_prekeys(
+            remote_identity
+        ) < 0) {
+        goto cleanup;
+    }
 
-if (gyrojet_x25519_generate_keypair(
-        alice_public,
-        alice_private
-    ) < 0) {
+    /*
+     * Gera EK_A.
+     */
+    if (gyrojet_x25519_generate_keypair(
+            message->ephemeral_public_key,
+            ephemeral_private
+        ) < 0) {
+        goto cleanup;
+    }
 
-    return -1;
+    /*
+     * Copia IK_A.
+     */
+    memcpy(
+        message->identity_public_key,
+        local_identity->identity_key.public_key,
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    );
+
+    /*
+     * Identificadores dos prekeys usados.
+     */
+    memcpy(
+        message->signed_prekey_id,
+        remote_identity->signed_prekey_id,
+        GYROJET_PREKEY_ID_SIZE
+    );
+
+    memcpy(
+        message->pq_signed_prekey_id,
+        remote_identity->pq_signed_prekey_id,
+        GYROJET_PREKEY_ID_SIZE
+    );
+
+    /*
+     * DH1 = DH(IK_A, SPK_B)
+     */
+    if (gyrojet_x25519_shared_secret(
+            local_identity->identity_key.private_key,
+            remote_identity->signed_prekey.public_key,
+            dh1
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * DH2 = DH(EK_A, IK_B)
+     */
+    if (gyrojet_x25519_shared_secret(
+            ephemeral_private,
+            remote_identity->identity_key.public_key,
+            dh2
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * DH3 = DH(EK_A, SPK_B)
+     */
+    if (gyrojet_x25519_shared_secret(
+            ephemeral_private,
+            remote_identity->signed_prekey.public_key,
+            dh3
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * PQKEM-ENC(PQSPK_B)
+     */
+    if (gyrojet_mlkem_encapsulate(
+            remote_identity->pq_signed_prekey.public_key,
+            message->pq_ciphertext,
+            pq_secret
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * DH1 || DH2 || DH3 || SS_PQ
+     */
+    memcpy(
+        key_material,
+        dh1,
+        sizeof(dh1)
+    );
+
+    memcpy(
+        key_material + sizeof(dh1),
+        dh2,
+        sizeof(dh2)
+    );
+
+    memcpy(
+        key_material + sizeof(dh1) + sizeof(dh2),
+        dh3,
+        sizeof(dh3)
+    );
+
+    memcpy(
+        key_material +
+        sizeof(dh1) +
+        sizeof(dh2) +
+        sizeof(dh3),
+        pq_secret,
+        sizeof(pq_secret)
+    );
+
+    if (gyrojet_pqxdh_kdf(
+            key_material,
+            sizeof(key_material),
+            result->shared_secret
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * AD = IK_A || IK_B
+     */
+    memcpy(
+        result->associated_data,
+        local_identity->identity_key.public_key,
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    );
+
+    memcpy(
+        result->associated_data +
+        GYROJET_X25519_PUBLIC_KEY_SIZE,
+        remote_identity->identity_key.public_key,
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    );
+
+    result->associated_data_size =
+        GYROJET_PQXDH_ASSOCIATED_DATA_SIZE;
+
+    result_code = 0;
+
+cleanup:
+
+    gyrojet_crypto_secure_zero(
+        ephemeral_private,
+        sizeof(ephemeral_private)
+    );
+
+    gyrojet_crypto_secure_zero(
+        dh1,
+        sizeof(dh1)
+    );
+
+    gyrojet_crypto_secure_zero(
+        dh2,
+        sizeof(dh2)
+    );
+
+    gyrojet_crypto_secure_zero(
+        dh3,
+        sizeof(dh3)
+    );
+
+    gyrojet_crypto_secure_zero(
+        pq_secret,
+        sizeof(pq_secret)
+    );
+
+    gyrojet_crypto_secure_zero(
+        key_material,
+        sizeof(key_material)
+    );
+
+    gyrojet_crypto_secure_zero(
+        signing_public_key,
+        sizeof(signing_public_key)
+    );
+
+    if (result_code != 0) {
+        gyrojet_pqxdh_message_clear(message);
+        gyrojet_pqxdh_result_clear(result);
+    }
+
+    return result_code;
 }
 
-if (gyrojet_x25519_generate_keypair(
-        bob_public,
-        bob_private
-    ) < 0) {
+int gyrojet_pqxdh_accept(
+    const gyrojet_identity_t *local_identity,
+    const gyrojet_pqxdh_message_t *message,
+    gyrojet_pqxdh_result_t *result
+)
+{
+    if (local_identity == NULL ||
+        message == NULL ||
+        result == NULL) {
+        return -1;
+    }
 
-    return -1;
+    memset(
+        result,
+        0,
+        sizeof(*result)
+    );
+
+    unsigned char dh1[
+        GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
+
+    unsigned char dh2[
+        GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
+
+    unsigned char dh3[
+        GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
+
+    unsigned char pq_secret[
+        GYROJET_MLKEM_SHARED_SECRET_SIZE
+    ];
+
+    unsigned char key_material[
+        4 * GYROJET_X25519_SHARED_SECRET_SIZE
+    ];
+
+    memset(dh1, 0, sizeof(dh1));
+    memset(dh2, 0, sizeof(dh2));
+    memset(dh3, 0, sizeof(dh3));
+    memset(pq_secret, 0, sizeof(pq_secret));
+    memset(key_material, 0, sizeof(key_material));
+
+    int result_code = -1;
+
+    /*
+     * Verifica se a mensagem usa o signed prekey
+     * atualmente ativo.
+     */
+    if (memcmp(
+            message->signed_prekey_id,
+            local_identity->signed_prekey_id,
+            GYROJET_PREKEY_ID_SIZE
+        ) != 0) {
+        goto cleanup;
+    }
+
+    if (memcmp(
+            message->pq_signed_prekey_id,
+            local_identity->pq_signed_prekey_id,
+            GYROJET_PREKEY_ID_SIZE
+        ) != 0) {
+        goto cleanup;
+    }
+
+    /*
+     * DH1 = DH(SPK_B, IK_A)
+     */
+    if (gyrojet_x25519_shared_secret(
+            local_identity->signed_prekey.private_key,
+            message->identity_public_key,
+            dh1
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * DH2 = DH(IK_B, EK_A)
+     */
+    if (gyrojet_x25519_shared_secret(
+            local_identity->identity_key.private_key,
+            message->ephemeral_public_key,
+            dh2
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * DH3 = DH(SPK_B, EK_A)
+     */
+    if (gyrojet_x25519_shared_secret(
+            local_identity->signed_prekey.private_key,
+            message->ephemeral_public_key,
+            dh3
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * PQKEM-DEC(PQSPK_B, CT)
+     */
+    if (gyrojet_mlkem_decapsulate(
+            local_identity->pq_signed_prekey.private_key,
+            message->pq_ciphertext,
+            pq_secret
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * DH1 || DH2 || DH3 || SS_PQ
+     */
+    memcpy(
+        key_material,
+        dh1,
+        sizeof(dh1)
+    );
+
+    memcpy(
+        key_material + sizeof(dh1),
+        dh2,
+        sizeof(dh2)
+    );
+
+    memcpy(
+        key_material + sizeof(dh1) + sizeof(dh2),
+        dh3,
+        sizeof(dh3)
+    );
+
+    memcpy(
+        key_material +
+        sizeof(dh1) +
+        sizeof(dh2) +
+        sizeof(dh3),
+        pq_secret,
+        sizeof(pq_secret)
+    );
+
+    if (gyrojet_pqxdh_kdf(
+            key_material,
+            sizeof(key_material),
+            result->shared_secret
+        ) < 0) {
+        goto cleanup;
+    }
+
+    /*
+     * AD = IK_A || IK_B
+     */
+    memcpy(
+        result->associated_data,
+        message->identity_public_key,
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    );
+
+    memcpy(
+        result->associated_data +
+        GYROJET_X25519_PUBLIC_KEY_SIZE,
+        local_identity->identity_key.public_key,
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    );
+
+    result->associated_data_size =
+        GYROJET_PQXDH_ASSOCIATED_DATA_SIZE;
+
+    result_code = 0;
+
+cleanup:
+
+    gyrojet_crypto_secure_zero(
+        dh1,
+        sizeof(dh1)
+    );
+
+    gyrojet_crypto_secure_zero(
+        dh2,
+        sizeof(dh2)
+    );
+
+    gyrojet_crypto_secure_zero(
+        dh3,
+        sizeof(dh3)
+    );
+
+    gyrojet_crypto_secure_zero(
+        pq_secret,
+        sizeof(pq_secret)
+    );
+
+    gyrojet_crypto_secure_zero(
+        key_material,
+        sizeof(key_material)
+    );
+
+    if (result_code != 0)
+        gyrojet_pqxdh_result_clear(result);
+
+    return result_code;
 }
 
-if (gyrojet_x25519_shared_secret(
-        alice_private,
-        bob_public,
-        alice_dh
-    ) < 0) {
+void gyrojet_pqxdh_message_clear(
+    gyrojet_pqxdh_message_t *message
+)
+{
+    if (message == NULL)
+        return;
 
-    return -1;
-}
-
-if (gyrojet_x25519_shared_secret(
-        bob_private,
-        alice_public,
-        bob_dh
-    ) < 0) {
-
-    return -1;
-}
-
-if (memcmp(
-        alice_dh,
-        bob_dh,
-        sizeof(alice_dh)
-    ) != 0) {
-
-    return -1;
-}
-
-if (gyrojet_mlkem_generate_keypair(
-        bob_mlkem_public,
-        bob_mlkem_private
-    ) < 0) {
-
-    return -1;
-}
-
-if (gyrojet_mlkem_encapsulate(
-        bob_mlkem_public,
-        ciphertext,
-        alice_mlkem_secret
-    ) < 0) {
-
-    return -1;
-}
-
-if (gyrojet_mlkem_decapsulate(
-        bob_mlkem_private,
-        ciphertext,
-        bob_mlkem_secret
-    ) < 0) {
-
-    return -1;
-}
-
-if (memcmp(
-        alice_mlkem_secret,
-        bob_mlkem_secret,
-        sizeof(alice_mlkem_secret)
-    ) != 0) {
-
-    return -1;
-}
-
-/*
- * This is intentionally only a primitive
- * interoperability test.
- *
- * It is NOT the final PQXDH KDF.
- */
-
-memcpy(
-    result->shared_secret,
-    alice_mlkem_secret,
-    sizeof(result->shared_secret)
-);
-
-result->associated_data_size = 0;
-
-gyrojet_crypto_secure_zero(
-    alice_private,
-    sizeof(alice_private)
-);
-
-gyrojet_crypto_secure_zero(
-    bob_private,
-    sizeof(bob_private)
-);
-
-gyrojet_crypto_secure_zero(
-    alice_dh,
-    sizeof(alice_dh)
-);
-
-gyrojet_crypto_secure_zero(
-    bob_dh,
-    sizeof(bob_dh)
-);
-
-gyrojet_crypto_secure_zero(
-    bob_mlkem_private,
-    sizeof(bob_mlkem_private)
-);
-
-gyrojet_crypto_secure_zero(
-    alice_mlkem_secret,
-    sizeof(alice_mlkem_secret)
-);
-
-gyrojet_crypto_secure_zero(
-    bob_mlkem_secret,
-    sizeof(bob_mlkem_secret)
-);
-
-return 0;
-
+    gyrojet_crypto_secure_zero(
+        message,
+        sizeof(*message)
+    );
 }
 
 void gyrojet_pqxdh_result_clear(
-gyrojet_pqxdh_result_t *result
+    gyrojet_pqxdh_result_t *result
 )
 {
-if (result == NULL)
-return;
+    if (result == NULL)
+        return;
 
-gyrojet_crypto_secure_zero(
-    result,
-    sizeof(*result)
-);
-
+    gyrojet_crypto_secure_zero(
+        result,
+        sizeof(*result)
+    );
 }
-
--- src/handshake/pqxdh.h
+++ src/handshake/pqxdh.h
@@ -2,31 +2,68 @@
 #define GYROJET_PQXDH_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+#include "../identity/identity.h"
 
 #define GYROJET_PQXDH_SHARED_SECRET_SIZE 32
+#define GYROJET_PQXDH_ASSOCIATED_DATA_SIZE 64
 
 typedef struct {
-unsigned char shared_secret[
-GYROJET_PQXDH_SHARED_SECRET_SIZE
-];
+    unsigned char identity_public_key[
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    ];
 
-unsigned char associated_data[64];
+    unsigned char ephemeral_public_key[
+        GYROJET_X25519_PUBLIC_KEY_SIZE
+    ];
 
-size_t associated_data_size;
+    unsigned char pq_ciphertext[
+        GYROJET_MLKEM_CIPHERTEXT_SIZE
+    ];
 
+    unsigned char signed_prekey_id[
+        GYROJET_PREKEY_ID_SIZE
+    ];
+
+    unsigned char pq_signed_prekey_id[
+        GYROJET_PREKEY_ID_SIZE
+    ];
+} gyrojet_pqxdh_message_t;
+
+typedef struct {
+    unsigned char shared_secret[
+        GYROJET_PQXDH_SHARED_SECRET_SIZE
+    ];
+
+    unsigned char associated_data[
+        GYROJET_PQXDH_ASSOCIATED_DATA_SIZE
+    ];
+
+    size_t associated_data_size;
 } gyrojet_pqxdh_result_t;
 
-int gyrojet_pqxdh_init(
-void
+int gyrojet_pqxdh_init(void);
+
+int gyrojet_pqxdh_initiate(
+    const gyrojet_identity_t *local_identity,
+    const gyrojet_identity_t *remote_identity,
+    gyrojet_pqxdh_message_t *message,
+    gyrojet_pqxdh_result_t *result
 );
 
-int gyrojet_pqxdh_run_test(
-gyrojet_pqxdh_result_t *result
+int gyrojet_pqxdh_accept(
+    const gyrojet_identity_t *local_identity,
+    const gyrojet_pqxdh_message_t *message,
+    gyrojet_pqxdh_result_t *result
+);
+
+void gyrojet_pqxdh_message_clear(
+    gyrojet_pqxdh_message_t *message
 );
 
 void gyrojet_pqxdh_result_clear(
-gyrojet_pqxdh_result_t *result
+    gyrojet_pqxdh_result_t *result
 );
 
 #endif
-
